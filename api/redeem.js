0460eb5add1cb3fd3f2fb4a543c058e15faa3aea
PashaDev <kimtha293@gmail.com>
2026-05-12 16:58:01 +0700

    Update redeem.js

--- api/redeem.js
+++ api/redeem.js
@@ -1,19 +1,9 @@
 export default function handler(req, res) {
-  const commands = ["redeem", "limit", "xp", "bonus"]
 
   const code = Math.random().toString(36).substring(2, 10).toUpperCase()
-  const command = commands[Math.floor(Math.random() * commands.length)]
 
-  // kalau kamu mau kirim ke bot via redeem command
-  const text = `.redeem ${code}`
-
-  const waNumber = "6285864588583"
-  const waLink = `https://wa.me/${waNumber}?text=${encodeURIComponent(text)}`
-
-  // OPTIONAL: kalau mau simpan ke log / DB nanti bisa di sini
-
-  res.writeHead(302, {
-    Location: waLink
+  res.status(200).json({
+    message: "copy kode ini lalu redeem di bot",
+    code: code
   })
-  res.end()
 }
